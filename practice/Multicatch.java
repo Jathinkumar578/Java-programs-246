f6bfb0c10a76f94028d606ac9176b0db28aadc8c
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-08-28 12:07:09 +0530

    Add files via upload

---
+++ practice/Multicatch.java
@@ -0,0 +1,14 @@
+public class Multicatch {
+   public static void main(String[] args) {
+       try {
+           int[] numbers = {10, 20};
+           int result = numbers[5];
+           } catch (ArithmeticException ae) {
+               System.out.println("Error caught: " + ae.getClass().getSimpleName());
+               }
+        catch(ArrayIndexOutOfBoundsException e){
+        System.out.println("Error caught: " + e.getClass().getSimpleName());
+        }
+        System.out.println("Program finishes successfully.");
+        }
+   }
