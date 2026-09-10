a113cfb65459222228210d424d9a7113481095ab
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 20:35:57 +0530

    Add files via upload

---
+++ assessment/string5.java
@@ -0,0 +1,12 @@
+
+public class String5 {
+    public static void main(String[] args) {
+            // last index find out the progrsm
+            String t = "anits college";
+            int ind  = t.lastIndexOf("anits");
+            int din =  t.lastIndexOf("college");
+            System.out.println(t);
+            System.out.println(ind);
+            System.out.println(din);
+    }
+}
