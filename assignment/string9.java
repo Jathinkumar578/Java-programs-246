fb19a42b65db2c7bdad22239b08875e50477a174
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 21:19:14 +0530

    Add files via upload

---
+++ assignment/string9.java
@@ -0,0 +1,9 @@
+
+public class String9 {
+    public static void main(String[] args) {
+        String z = " jathinkumar246";
+        String [] gap = z.split(",");
+        System.out.println("originalstring is " + z);
+        System.out.println("spilt the string" + z);
+    }
+}
