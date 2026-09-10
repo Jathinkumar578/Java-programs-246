39f9f03719abe92a139c94dd243d3e809acbf588
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 21:02:21 +0530

    Add files via upload

---
+++ observation/exception3.java
@@ -0,0 +1,17 @@
+
+public class Exception3 {
+    public static void main(String[] args) {
+        String man = "Jathin";
+        try {
+            System.out.println(man.charAt(9));
+        } catch (StringIndexOutOfBoundsException sioobe) {
+            // TODO: handle exception
+            System.out.println("we caught an exception called sioobe" + sioobe.getMessage());
+
+        }
+        finally{
+            System.out.println("we get sioobe exception");
+        }
+    }
+
+}
