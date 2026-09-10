1de8eee63e69f0d81e4e28a8cd24999c2f340552
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 20:59:09 +0530

    Add files via upload

---
+++ observation/exception1.java
@@ -0,0 +1,12 @@
+
+public class Exception1 {
+    public static void main(String[] args) {
+        String name  = null;
+        try {
+            System.out.println(name.length());
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("we get an nullpointer exception" + e.getMessage());
+        }
+    }
+}
