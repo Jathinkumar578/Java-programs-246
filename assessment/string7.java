8466eb93dd5bbd98c39664582c63a0e58e62a37d
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 20:38:41 +0530

    Add files via upload

---
+++ assessment/string7.java
@@ -0,0 +1,9 @@
+
+public class String7 {
+    public static void main(String[] args) {
+        String o = "hello windows";
+        String re = o.replace("e","a" );
+        System.out.println(o);
+        System.out.println(re);
+    }
+}
