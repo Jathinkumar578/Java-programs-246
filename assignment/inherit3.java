6b671b2d9a1acc9a397c94f401088adfabaa3350
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 20:42:36 +0530

    Add files via upload

---
+++ assignment/inherit3.java
@@ -0,0 +1,27 @@
+class animal{
+String name = " lion";
+void ani(){
+    System.out.println(" lion is the my fav animal");
+}
+}
+class bird extends animal{
+    String names = " parrot , sparrow";
+    void bi(){
+        System.out.println("parrot is my fav bird");// multi inheritanceis executed
+    }
+}
+class sea extends animal {
+    float marks = 99.23f;
+    void mar(){
+        System.out.println(" you got 1 st rank");
+    }
+}
+public class Inherit3 {
+    public static void main(String[] args) {
+// create an object for 3rd class to super class 
+        sea as = new sea();
+        as.mar();
+        as.ani();
+        as.ani();
+    }
+}
