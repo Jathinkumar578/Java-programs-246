62b9fceac07ddd684f9770b126b5ebe5f8b18941
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 21:20:38 +0530

    Add files via upload

---
+++ assignment/string10.java
@@ -0,0 +1,10 @@
+
+public class String10 {
+    public static void main(String[] args) {
+        String t = "JATHIN";
+        String i = "jathin";
+        String e = "jathin";
+    System.out.println("t is euals to i is "  + t.equals(i));  
+    System.out.println("t is euals to  e is " + t.equals(e));
+  }
+}
