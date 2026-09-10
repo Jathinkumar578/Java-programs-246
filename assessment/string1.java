f138f2fb588d6896e46663950a8a39f3386f3275
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 20:26:13 +0530

    Add files via upload

---
+++ assessment/string1.java
@@ -0,0 +1,9 @@
+
+public class String1 {
+    public static void main(String[] args) {
+        String a = "Jathin kumar 246";
+        int length = a.length();
+        System.out.println("String is" + a);
+        System.out.println("string length is" + length);
+    }
+}
