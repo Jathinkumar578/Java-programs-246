4daa719cf3bf3ca4c84566ac071aa0407b0a9e43
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-10 21:10:15 +0530

    Add files via upload

---
+++ observation/exception9.java
@@ -0,0 +1,15 @@
+
+public class Exception9{
+   public static void main(String[] args) {
+    try {
+        throw new TypeNotPresentException("missing class of not same type", new ClassNotFoundException());
+    } catch (TypeNotPresentException tnpe) {
+        // TODO: handle exception
+        System.out.println("the exception is "  + tnpe.getMessage());
+
+    }
+    finally{
+        System.out.println("catched done");
+    }
+   } 
+}
