88e424369ea0c78d3ff45c6a2e976ee5e29bc3c6
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-24 18:40:39 +0530

    Delete assignment/string7.java

--- assignment/string7.java
+++
@@ -1,9 +0,0 @@
-
-public class String7 {
-    public static void main(String[] args) {
-        String o = "hello windows";
-        String re = o.replace("e","a" );
-        System.out.println(o);
-        System.out.println(re);
-    }
-}
