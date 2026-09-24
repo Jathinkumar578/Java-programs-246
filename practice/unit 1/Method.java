f66d51fa3b09d66251d42de63139ffa5aaea964e
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-24 20:37:25 +0530

    Add Method class with a simple print method

---
+++ practice/unit 1/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    // Method without parameters and without return value
+    void parameters() {
+        System.out.println("Jathin");
+    }
+
+    public static void main(String[] args) {
+        Method pr = new Method();
+        pr.parameters();
+    }
+}
