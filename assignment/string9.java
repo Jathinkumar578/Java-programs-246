01ef0127f5a4ea39a95dfb7c5ec7f135648fb589
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-24 18:47:13 +0530

    Delete assignment/string9.java

--- assignment/string9.java
+++
@@ -1,9 +0,0 @@
-
-public class String9 {
-    public static void main(String[] args) {
-        String z = " jathinkumar246";
-        String [] gap = z.split(",");
-        System.out.println("originalstring is " + z);
-        System.out.println("spilt the string" + z);
-    }
-}
