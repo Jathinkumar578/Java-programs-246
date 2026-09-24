38ffba787b680fea9b844f62f41cd808f027c7f5
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-24 18:39:49 +0530

    Add String7 class to demonstrate string replacement

---
+++ assignment/strings/String7.java
@@ -0,0 +1,8 @@
+public class String7 {
+    public static void main(String[] args) {
+        String o = "hello windows";
+        String re = o.replace("e","a" );
+        System.out.println(o);
+        System.out.println(re);
+    }
+}
