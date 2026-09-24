eafba5a1cbec28c93e59399951768a3b3bd36f95
Jathinkumar578 <peramballijathinkumar.25.csm@anits.edu.in>
2026-09-24 20:42:36 +0530

    Delete practice/NPE.java

--- practice/NPE.java
+++
@@ -1,8 +0,0 @@
-public class NPE{
-     public static void main(String[] args) {
-    String Name = null; // The reference points to nothing
-    // This line will crash the program with a NullPointerException
-    int length = Name.length();
-    System.out.println("Name length is: " + length);
-    }
-}
